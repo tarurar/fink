31fe95f9eb6febc9935a60247525db17a5a6bfcd
atarutin <142044+tarurar@users.noreply.github.com>
2025-06-21 23:15:37 +0300

    test: fix Dependency tests

--- src/Fink.Abstractions.Tests/DependencyTests.cs
+++ src/Fink.Abstractions.Tests/DependencyTests.cs
@@ -5,36 +5,18 @@ public class DependencyTests
     [Fact]
     public void When_ParentDependencyIsNull_Then_PathIsCreatedWithSingleSegment()
     {
-        Dependency dependency = new(new("dependency"));
+        Dependency dependency = new(new("dependency"), new DependencyVersion("1.0"));
 
         DependencyPathSegment rootSegment = Assert.Single(dependency.Path.Segments);
-        Assert.Equal("dependency", rootSegment.ToString());
+        Assert.Equal("dependency 1.0", rootSegment.ToString());
     }
 
     [Fact]
     public void When_ParentDependencyProvided_Then_PathIsCreatedWithMultipleSegments()
     {
-        Dependency parentDependency = new(new("parent"));
-        Dependency dependency = new(new("dependency"), ParentDependency: parentDependency);
+        Dependency parentDependency = new(new("parent"), new DependencyVersion("1.0"));
+        Dependency dependency = new(new("dependency"), new DependencyVersion("2.0"), ParentDependency: parentDependency);
 
         Assert.Equal(2, dependency.Path.Segments.Count);
-    }
-
-    [Fact]
-    public void When_VersionIsNull_Then_PathSegmentIsCreatedWithoutVersion()
-    {
-        Dependency dependency = new(new("dependency"));
-
-        DependencyPathSegment rootSegment = Assert.Single(dependency.Path.Segments);
-        Assert.Equal("dependency", rootSegment.ToString());
-    }
-
-    [Fact]
-    public void When_VersionIsProvided_Then_PathSegmentIsCreatedWithVersion()
-    {
-        Dependency dependency = new(new("dependency"), new("1.0.0"));
-
-        DependencyPathSegment rootSegment = Assert.Single(dependency.Path.Segments);
-        Assert.Equal("dependency 1.0.0", rootSegment.ToString());
     }
 }
